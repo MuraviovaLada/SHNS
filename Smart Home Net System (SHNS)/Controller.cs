ebea2f886de39d6b72e8af24eb86ad58728fc9d0
Лада Максимовна Муравьёва <s0135238@edu.kubsu.ru>
2021-03-21 13:41:53 +0300

    Creation of the singleton Controller class

---
+++ Smart Home Net System (SHNS)/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Home_Net_System__SHNS_
+{
+    public sealed class Controller
+    {
+        // Controller is a unique appliance (and singleton-type class),
+        // which receives inputs from the "Control Panel" and the sensors,
+        // processes them and sends instructions to executing devices
+
+        private static Controller instance = null;
+        private static readonly object padlock = new object();
+
+        Controller()
+        {
+        }
+
+        public static Controller Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Controller();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
