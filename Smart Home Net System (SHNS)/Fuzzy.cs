e377458e93e721d65f8a757fe3de39f04b6b8357
Лада Максимовна Муравьёва <s0135238@edu.kubsu.ru>
2021-03-21 18:41:05 +0300

    Installed nuget; Nuget Console - installed cs-fuzzy-logic

---
+++ Smart Home Net System (SHNS)/Fuzzy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FuzzyLogic;
+using FuzzyLogic.Memberships;
+
+namespace Smart_Home_Net_System__SHNS_
+{
+    class Fuzzy
+    {
+        RuleInferenceEngine rie = new RuleInferenceEngine();
+
+    }
+}
